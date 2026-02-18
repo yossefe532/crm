efaa7350dbdb5730477318ae78b29c5d23322927
yossefe532 <yossefe2020a@gmail.com>
2026-02-18 17:48:16 +0200

    fix: hardcode backend url fallback to solve vercel 500 error

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -11,6 +11,15 @@ const nextConfig = {
         hostname: "i.postimg.cc"
       }
     ]
+  },
+  async rewrites() {
+    const backendOrigin = process.env.BACKEND_ORIGIN || "http://localhost:4000"
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${backendOrigin}/api/:path*`
+      }
+    ]
   }
 }
 
