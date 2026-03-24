32f6ed212c17b24deeba9bdf8b35519bdef3d29b
yossefe532 <yossefe2020a@gmail.com>
2026-03-24 23:12:05 +0200

    fix(prisma): force runtime datasource url from env resolver

--- dist/prisma/client.js
+++ dist/prisma/client.js
@@ -2,8 +2,16 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.prisma = void 0;
 const client_1 = require("@prisma/client");
+const env_1 = require("../config/env");
 const globalForPrisma = global;
-exports.prisma = globalForPrisma.prisma || new client_1.PrismaClient();
+exports.prisma = globalForPrisma.prisma ||
+    new client_1.PrismaClient({
+        datasources: {
+            db: {
+                url: env_1.env.databaseUrl,
+            },
+        },
+    });
 if (process.env.NODE_ENV !== "production") {
     globalForPrisma.prisma = exports.prisma;
 }
--- src/prisma/client.ts
+++ src/prisma/client.ts
@@ -1,8 +1,17 @@
 import { PrismaClient } from "@prisma/client"
+import { env } from "../config/env"
 
 const globalForPrisma = global as unknown as { prisma?: PrismaClient }
 
-export const prisma = globalForPrisma.prisma || new PrismaClient()
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    datasources: {
+      db: {
+        url: env.databaseUrl,
+      },
+    },
+  })
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma
